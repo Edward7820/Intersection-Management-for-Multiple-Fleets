6313b84807c4477b94af9fef9f31af7ca0cfa5c9
Edward7820 <edward910530@gmail.com>
2023-04-27 12:43:13 +0800

    debug main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import itertools
 import json
 import zenoh
 from datetime import datetime
-from API.Vehicle import MyVehicle, Leader
+# from API.Vehicle import MyVehicle, Leader
 import os, signal
 from multiprocessing import Process, Array
 from typing import List, Dict, Tuple
@@ -26,24 +26,26 @@ def run_vehicle(veh_num: int, pid: int, lane_id: int, des_lane_id: int, fid: int
                 if r != cur_round:
                     continue
                 waiting = False
-        
-        print(cur_round)
+                
+        # time.sleep(5)
+        # print(cur_round, args.delta_t)
         cur_round += 1
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("--input_file",type=str, default="input.txt")
+    parser.add_argument("--input_file",type=str, default="sample_input")
     parser.add_argument("--delta_t",type=float, default=0.1)
     parser.add_argument("--max_speed",type=float,default=16)
     parser.add_argument("--max_acceleration",type=float,default=3)
     parser.add_argument("--min_acceleration",type=float,default=-3)
+    global args
     args = parser.parse_args()
 
     with open(args.input_file, 'r') as f:
         input_words = f.read()
     input_lines = input_words.split('\n')
-    basic_info = input_lines.split(' ')
+    basic_info = input_lines[0].split(' ')
     fleets_num = int(basic_info[0])
     veh_num = int(basic_info[1])
     rounds = Array('i', [0]*veh_num)
@@ -72,7 +74,7 @@ def main():
             vel_y = float(l[4])
             acc_x = float(l[5])
             acc_y = float(l[6])
-            fleets[i]['vehicle'].append({'vid':vid, 'location':(loc_x,loc_y),
+            fleets[i]['vehicles'].append({'vid':vid, 'location':(loc_x,loc_y),
                 'velocity':(vel_x,vel_y), 'acceleration':(acc_x,acc_y)})
             line_index += 1
             veh_proc = Process(target=run_vehicle, args=(veh_num, len(veh_processes), 
@@ -87,6 +89,6 @@ def main():
         for proc in veh_processes:
             proc.join(timeout=2)
 
-if __init__ == '__main__':
+if __name__ == '__main__':
     main()
     
